be2b15407beb015d88171f125bfd03917cc581d7
Jason Tam <jasontamemail@gmail.com>
2020-01-14 15:36:52 -0500

    update model diagram and save insert dummy data sql

---
+++ InsertTestData.sql
@@ -0,0 +1,4 @@
+INSERT INTO `stamps`.`Sets` (`ID`, `Country`, `Name`, `Description`, `Ordinality`) VALUES ('1999-1', 'CHN', 'Year of the Rabbit 1999', 'Drawings celebrating the year of the Rabbit 1999', '2');
+INSERT INTO `stamps`.`Sets` (`ID`, `Country`, `Name`, `Description`, `Ordinality`) VALUES ('2000-17', 'CHN', 'Farmers', 'Celebrating Farming', '4');
+INSERT INTO `stamps`.`Sets` (`ID`, `Country`, `Name`, `Description`, `Ordinality`) VALUES ('2011-16', 'CHN', 'New Year Emblems', 'Emblems for the Chinese New Year', '1');
+INSERT INTO `stamps`.`Sets` (`ID`, `Country`, `Name`, `Description`, `Ordinality`) VALUES ('2019-23', 'CHN', 'Progress in China', 'National Progress in Major Sectors', '5');
